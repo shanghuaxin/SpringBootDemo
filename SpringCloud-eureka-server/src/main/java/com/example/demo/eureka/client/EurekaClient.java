c48c716150249a1df83005636d38c0879ab8e83e
shanghuaxin <shanghuaxin@github.com>
2018-03-17 16:17:37 +0800

    test

---
+++ SpringCloud-eureka-server/src/main/java/com/example/demo/eureka/client/EurekaClient.java
@@ -0,0 +1,14 @@
+package com.example.demo.eureka.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@EnableEurekaClient
+@RestController
+public class EurekaClient {
+
+	@RequestMapping("/hi")
+	public String testEurekaClient() {
+		return "hello, eureka client";
+	}
+}
